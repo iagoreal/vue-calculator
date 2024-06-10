f764c21018be274cc4226c10641c45f318351d7b
Iago Real <iagovreal@gmail.com>
2024-06-10 17:07:36 -0300

    code base

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,47 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+  export default {
+    data(){
+      return {
+      currentDisplay: "",
+      buttons: ["1", "2", "3", "+", "4", "5", "6", "-", "7", "8", "9", "*", "0", ".", "/"]
+      }
+    },
+    methods: {
+      addToDisplay(button) {
+      this.currentDisplay += button;
+    },
+    clearcurrenteDisplay() {
+      this.currentDisplay = "";
+    },
+    calculate() {
+      try {
+        this.currentDisplay = eval(this.currentDisplay);
+      } catch (error) {
+        this.currentDisplay = "Error";
+        }
+      }
+    }
+  }
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <input type="text" v-model="currentDisplay" readonly> 
+  <div class="buttons">
+    <button v-for="button in buttons" :key="button" @click="addToDisplay(button)">{{ button}}</button>
+    <button @click="clearcurrenteDisplay">C</button>
+    <button @click="calculate">=</button>
+  </div>
+  </template>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
-</template>
-
-<style scoped>
-header {
-  line-height: 1.5;
+<style>
+button{
+  width: 20em;
+  height: 5em;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+currentDisplay{
+  width: 20em;
+  height: 5em;
 }
 </style>
