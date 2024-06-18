02b666e9e4117341206f3e5d2773f78ec1f62fbc
Felipe CArneiro Ribeiro <felipecnrb@gmail.com>
2024-06-18 16:27:55 -0300

    Feature/style calculator (#1)
    * add style
    
    * add space '0' and '='

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,12 @@
   <div id="App">
     <input type="text" :value="display" readonly>
     <div class="buttons">
-      <button v-for="button in buttons" :key="button" @click="onButtonClick(button)">
+      <button v-for="button in buttons" :key="button" @click="onButtonClick(button)"
+              :class="{ 'zero': button === '0', 'equal': button === '=' }">
         {{ button }}
       </button>
       <button @click="onClearDisplay">C</button>
-      <button @click="onCalculate">=</button>
+      <button @click="onCalculate" class="equal">=</button>
     </div>
   </div>
 </template>
@@ -43,5 +44,80 @@ export default defineComponent({
 </script>
 
 <style scoped>
+ #App {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 50px;
+  padding: 20px;
+  border: 2px solid #333; /* Adiciona a borda em volta da calculadora */
+  border-radius: 10px; /* Borda arredondada */
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); /* Adiciona uma leve sombra */
+  background-color: #f9f9f9; /* Fundo branco para destaque */
+}
+
+#display {
+  background-color: #333;
+  color: white;
+  font-size: 2em;
+  width: 100%;
+  text-align: right;
+  padding: 20px;
+  border: 1px solid #666;
+  border-radius: 5px;
+  box-sizing: border-box;
+}
+
+input[type="text"] {
+  font-size: 1.5em;
+  width: 100%;
+  text-align: right;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  margin-bottom: 10px;
+  box-sizing: border-box;
+}
+
+.buttons {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 10px;
+}
+
+button {
+  background-color: #f0f0f0;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  font-size: 1.2em;
+  padding: 20px;
+  cursor: pointer;
+  transition: background-color 0.3s;
+}
+
+button:hover {
+  background-color: #ddd;
+}
+
+button:active {
+  background-color: #ccc;
+}
+
+button:nth-child(4n),
+button:nth-child(4n-1),
+button:nth-child(4n-2),
+button:nth-child(4n-3) {
+  width: 100%;
+}
+
+/* Especificamente para o botão zero (0) */
+button.zero {
+  grid-column: span 2;
+}
+
+/* Especificamente para o botão de igual (=) */
+button.equal {
+  grid-column: span 3;
+}
 
 </style>
